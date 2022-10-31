0104f9377790112aca5ff2b79f990ac1ce7773f1
resurrectiontent <asvoskresenskii@gmail.com>
2022-10-31 19:21:09 +0300

    doc memory criteria

--- memory/base.py
+++ memory/base.py
@@ -10,26 +10,37 @@ class MemoryCriterion(ABC):
     _moves: List[Solution]
     _move_idx: Set[TMoveId]
     # TODO: Consider more elegant implementation
-    _move_id_getter: Callable[[Solution], TMoveId]
+    _solution_id_getter: Callable[[Solution], TMoveId]
 
+    # TODO: consider taking set of solutions to broaden implementation possibilities.
     @abstractmethod
     def _criterion(self, x_idx: Set[TMoveId]) -> Set[TMoveId]:
+        """
+        Returns a set of ids, which corresponds to this criterion type. The set can have intersection with x_idx
+        :param x_idx:
+        :return:
+        """
         ...
 
     def memorize(self, move: Solution):
         self._moves.append(move)
-        self._move_idx.update(self._move_id_getter(move))
+        self._move_idx.update(self._solution_id_getter(move))
 
     def filter(self, x: Iterable[Solution]) -> Iterable[Solution]:
         good_idx, move_idx = self._get_all_and_good_move_idx(x)
         return [move for move, idx in zip(x, move_idx) if idx in good_idx]
 
     def _filter_list_idx(self, x: Iterable[Solution]) -> Set[int]:
+        """
+        Like filter, but uses solutions ids in ordered list, rather then solution ids via _solution_id_getter
+        :param x:
+        :return:
+        """
         good_idx, move_idx = self._get_all_and_good_move_idx(x)
         return {i for i, idx in enumerate(move_idx) if idx in good_idx}
 
     def _get_all_and_good_move_idx(self, x: Iterable[Solution]):
-        move_idx = [self._move_id_getter(move) for move in x]
+        move_idx = [self._solution_id_getter(move) for move in x]
         good_idx = self._criterion(set(move_idx))
         return good_idx, move_idx
 
@@ -55,6 +66,9 @@ class MemoryCriterion(ABC):
 
 
 class CumulativeMemoryCriterion(MemoryCriterion):
+    """
+    Used to accumulate multiple memory criteria
+    """
     def __init__(self, operation: Callable[[Set[TMoveId], Set[TMoveId]], Set[TMoveId]], *criteria):
         assert len(criteria) > 0
 
