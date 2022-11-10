60fbd698cd5e2fb6b713f1503e2888806f564534
resurrectiontent <asvoskresenskii@gmail.com>
2022-11-11 01:36:33 +0300

    improve AggregatedSolutionQuality

--- solution/quality/aggregated.py
+++ solution/quality/aggregated.py
@@ -1,24 +1,30 @@
+from functools import partialmethod
+from operator import eq, lt
 from typing import Callable, Iterable
+
+import numpy as np
+from numpy import ndarray
 
 from solution.quality.base import BaseSolutionQualityMetric
 
 
 class AggregatedSolutionQuality(BaseSolutionQualityMetric):
-
     def __init__(self, name: str, *metrics, aggregation: [Callable[[Iterable[bool]], bool]]):
         value_str = '\n'.join(list(map(str, metrics)))
         super().__init__(name, value_str)
 
-        self.metrics = list(metrics)
-        self.aggregation = aggregation
+        self._metrics = list(metrics)
+        self._aggregation = aggregation
 
-        ...
+    @property
+    def _float_ndarray(self) -> ndarray:
+        return np.array([float(m) for m in self._metrics])
 
-    def _equals_to(self, other) -> bool:
-        pass
+    def _cmp_agg(self, other: 'AggregatedSolutionQuality', cmp):
+        return self._aggregation(cmp(self._float_ndarray, other._float_ndarray))
 
-    def _less_than(self, other) -> bool:
-        pass
+    _equals_to = partialmethod(_cmp_agg, cmp=eq)
+    _less_than = partialmethod(_cmp_agg, cmp=lt)
 
 # TODO: Custom aggregation with aggregation:[Callable[[Iterable[bool]], bool]
 # TODO: Shortcut for all, any, etc.
