c34baa80adf1a654409ab9dcc4426d48fbae803a
resurrectiontent <asvoskresenskii@gmail.com>
2022-11-14 16:55:52 +0300

    finish SolutionQualityFactory

--- solution/quality/factory.py
+++ solution/quality/factory.py
@@ -1,3 +1,4 @@
+from functools import partialmethod
 from typing import Callable, Iterable, Optional
 
 from numpy import ndarray
@@ -6,11 +7,20 @@ from solution.quality.base import BaseSolutionQualityInfo
 
 
 class SolutionQualityFactory:
+    _factory = Callable[[ndarray], BaseSolutionQualityInfo]
+
     # TODO: abstract *metrics creation to utility functions
     #  consider introducing SolutionQualityMetric or just partial for BaseSolutionQualityInfo-inherited ctors
     def __init__(self, *metrics: Callable[[ndarray], BaseSolutionQualityInfo],
                  metrics_aggregation: Optional[Callable[[Iterable[BaseSolutionQualityInfo]],
                                                         BaseSolutionQualityInfo]] = None):
-        assert len(metrics) == 1 or metrics_aggregation is not None
-        # TODO: finish constructor
-        ...
+        assert len(metrics) == 1 or metrics_aggregation is not None and len(metrics) > 1
+
+        if len(metrics) == 1:
+            self._factory,  = metrics
+        else:
+            def factory(x: ndarray):
+                return metrics_aggregation([m(x) for m in metrics])
+            self._factory = factory
+
+    __call__ = partialmethod(_factory)
