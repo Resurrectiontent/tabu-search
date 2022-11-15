396b04211d57cf8b157bc6e10de8f596c7af3714
resurrectiontent <asvoskresenskii@gmail.com>
2022-11-15 20:21:22 +0300

    develop SolutionQualityFactory

--- solution/factory.py
+++ solution/factory.py
@@ -1,10 +1,19 @@
+from functools import partial
 from typing import Callable
 
 from solution.base import Solution
-
-# TODO: implement
+from solution.id import SolutionId
 
 
 class SolutionFactory:
-    def __init__(self, solution_id_getter: Callable[[Solution], float], position_quality_getter: Callable):
+    def __init__(self, position_quality_getter: Callable):
+        self._initialized = False
+        self._solution_id_factory = None
+        # TODO: initialize quality factory
         ...
+
+    # TODO: impement factory call
+
+    def for_solution_generator(self, generator_name: str):
+        self._solution_id_factory = partial(SolutionId, generator_name)
+        self._initialized = True
