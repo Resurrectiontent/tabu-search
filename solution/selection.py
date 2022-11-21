ed5da5bcb3d8710de7ed05e6d9add0469f0cb5f0
resurrectiontent <asvoskresenskii@gmail.com>
2022-11-21 19:13:07 +0300

    develop SolutionSelection

--- solution/selection.py
+++ solution/selection.py
@@ -1,5 +1,5 @@
 from _operator import itemgetter
-from typing import Callable, Iterable, List
+from typing import Callable, Iterable
 
 from sortedcontainers import SortedList
 
@@ -7,10 +7,22 @@ from solution.base import Solution
 
 
 class SolutionSelection:
-    _indices_selector: Callable[[int], Iterable[int]]
+    _idx_selector: Callable[[int], int]
 
-    def __init__(self, indices_selector: Callable[[int], Iterable[int]] = None):
-        self._indices_selector = indices_selector if indices_selector is not None else range
+    def __init__(self, idx_selector: Callable[[int], int] = None):
+        """
+        Initializes solution selection strategy.
+        :param idx_selector: A function to select solution from the best.
+        It takes length of solution space and returns an index of solution to select
+        (0 - actual best with higher quality, 1 - second best, etc.).
+        Defaults to selection of the best solution.
+        """
+        self._idx_selector = idx_selector if idx_selector is not None else lambda: 0
 
-    def select_n_solutions(self, solutions: SortedList[Solution], n: int) -> List[Solution]:
-        return itemgetter(*self._indices_selector(n))(solutions)
+    def select_n_solutions(self, solutions: SortedList[Solution]) -> Iterable[Solution]:
+        """
+        Selects the "best" solution from ordered `solutions`. "Best" means defined by solution selection strategy.
+        :param solutions: `SortedList` of `Solution` objects ordered by ascending quality (best - last).
+        :return: The "best" solution in the collection.
+        """
+        return itemgetter(self._idx_selector(len(solutions)))(list(reversed(solutions)))
