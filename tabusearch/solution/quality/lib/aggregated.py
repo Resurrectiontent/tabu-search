eed51fbefa8eafe468ff7698dea94e28c1f5a307
resurrectiontent <asvoskresenskii@gmail.com>
2023-03-23 20:12:36 +0300

    add ignored code

---
+++ tabusearch/solution/quality/lib/aggregated.py
@@ -0,0 +1,51 @@
+from functools import partial
+from numbers import Number
+from typing import Union, Iterable, Callable
+
+from numpy import ndarray
+from numpy.typing import NDArray
+
+from tabusearch.solution.quality.aggregated import BaseAggregatedSolutionQualityInfo, \
+    CompareAggregatedSolutionQualityInfo, \
+    AggregateComparisonSolutionQualityInfo
+from solution.quality import BaseSolutionQualityInfo
+
+
+# TODO: add docstring
+
+
+def sum_metrics_aggregation(name: str, weights: Union[NDArray[Number], Iterable[Number], None] = None) \
+        -> Callable[[Iterable[BaseSolutionQualityInfo]], BaseAggregatedSolutionQualityInfo]:
+    if weights is None:
+        aggregation = sum
+
+    elif isinstance(weights, ndarray):
+        def aggregation(metric_values: Union[NDArray[float], Iterable[float]]):
+            if isinstance(metric_values, ndarray):
+                return metric_values * weights
+            return sum([i * j for i, j in zip(metric_values, list(weights))])
+
+    else:
+        def aggregation(metric_values: Union[NDArray[float], Iterable[float]]):
+            metric_values = list(metric_values) if isinstance(metric_values, ndarray) else metric_values
+            return sum([i * j for i, j in zip(metric_values, list(weights))])
+
+    return partial(CompareAggregatedSolutionQualityInfo, name=name, aggregation=aggregation)
+
+
+def per_metric_comparison_aggregation(name: str, aggregation: Union[str, Callable[[Iterable[bool]], bool]] = 'all'):
+    def get_aggregation(s: str):
+        if s == 'all':
+            return all
+        if s == 'any':
+            return any
+        if s == 'most':
+            return lambda l: len([None for x in l if x]) / len(l)
+        raise ValueError(f'Argument aggregation should be "all", "any", "most" or callable. Was "{s}".')
+
+    try:
+        aggregation = get_aggregation(aggregation) if isinstance(aggregation, str) else aggregation
+    except ValueError as ve:
+        raise ve
+
+    return partial(AggregateComparisonSolutionQualityInfo, name=name, aggregation=aggregation)
---
+++ tabusearch/solution/quality/lib/single.py
@@ -0,0 +1,25 @@
+# TODO: add simple non-aggregated metrics' shortcuts:
+#  sum/weighted sum
+#  ? custom function ?
+from functools import partial
+from numbers import Number
+from typing import Iterable, Callable
+
+import numpy as np
+from numpy import ndarray
+from numpy.typing import NDArray
+
+from tabusearch.solution.quality.single import SolutionQualityInfo
+
+
+def sum_metric(name: str, weights: NDArray[Number] | Iterable[Number] | None = None, **kwargs) \
+        -> Callable[[NDArray[Number]], SolutionQualityInfo]:
+    weights = weights and (weights if isinstance(weights, ndarray) else np.array(weights))
+    float_ = weights and (lambda x: (x * weights).sum()) or np.sum
+
+    return partial(SolutionQualityInfo, name=name, float_=float_, **kwargs)
+
+
+def custom_metric(name: str, evaluation: Callable[[NDArray[Number]], float], **kwargs) \
+        -> Callable[[NDArray[Number]], SolutionQualityInfo]:
+    return partial(SolutionQualityInfo, name=name, float_=evaluation, **kwargs)
