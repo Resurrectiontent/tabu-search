0928597d286fadf2ddec7af5e93896a0ca6d7fcb
resurrectiontent <asvoskresenskii@gmail.com>
2023-03-25 23:22:43 +0300

    naming + future plans

--- tests/scheduling/neighbourhood_tests.py
+++ tests/scheduling/neighbourhood_tests.py
@@ -2,13 +2,15 @@ import numpy as np
 import pytest
 from matplotlib import pyplot as plt
 
-from scheduling.neighbourhood import variable_partitioning_resource_neighbourhood
+from scheduling.resource_neighbourhood import variable_partitioning_resource_neighbourhood
 from tabusearch import TabuSearch
 from tabusearch.solution.base import Solution
 from tests.scheduling.fixtures import setup_schedule, setup_toolbox, setup_wg, setup_contractors, setup_worker_pool
+
 
 def test_resource_neighbourhood(setup_schedule, setup_toolbox):
     init_schedule = setup_toolbox.schedule_to_chromosome(schedule=setup_schedule)
 
     neighbourhood = variable_partitioning_resource_neighbourhood(init_schedule, setup_toolbox.get_worker_reqs(), 15, 30)
-    pass
+    # TODO: think on more accurate assertions
+    assert len(neighbourhood) > 10
