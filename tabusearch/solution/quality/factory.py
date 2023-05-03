7640202faa0bdba7898cf9e4a84cebc59994c8f9
resurrectiontent <asvoskresenskii@gmail.com>
2023-05-04 00:47:13 +0300

    start quality factory extension

--- tabusearch/solution/quality/factory.py
+++ tabusearch/solution/quality/factory.py
@@ -6,7 +6,11 @@ from tabusearch.typing_ import TData
 
 
 class SolutionQualityFactory(Generic[TData]):
-    _factory: Callable[[list[TData]], Iterable[BaseSolutionQualityInfo]]
+    _is_aggregated: bool
+
+    _metrics: list[Callable[[list[TData]], list[BaseSolutionQualityInfo]]]
+    _metrics_aggregation: Callable[[Iterable[Iterable[BaseSolutionQualityInfo]]],
+                                   Iterable[BaseAggregatedSolutionQualityInfo]] | None
 
     # TODO: consider moving ctor logics to helper functions
     #  and taking final `(ndarray) -> BaseSolutionQualityInfo` callable as an __init__ argument
@@ -14,19 +18,54 @@ class SolutionQualityFactory(Generic[TData]):
                  metrics_aggregation: Callable[[Iterable[Iterable[BaseSolutionQualityInfo]]],
                                                Iterable[BaseAggregatedSolutionQualityInfo]]
                                       | None = None):
-        assert len(metrics) == 1 or metrics_aggregation is not None and len(metrics) > 1
+        match metrics, metrics_aggregation:
+            case [_], None:
+                self._is_aggregated = False
+            case [_, _, *_], _:
+                self._is_aggregated = True
+            case _, _:
+                raise Exception('SolutionQualityFactory should be initialised either with one metric '
+                                'or with several metrics and an aggregation.')
 
-        match metrics:
-            case [single_metric]:
-                self._factory = single_metric
-            case _:
-                def factory(x: list[TData]) -> Iterable[BaseSolutionQualityInfo]:
-                    return metrics_aggregation(zip(*[m(x) for m in metrics]))
-                self._factory = factory
+        self._metrics = list(metrics)
+        self._metrics_aggregation = metrics_aggregation
 
     def __call__(self, x: list[TData]) -> Iterable[BaseSolutionQualityInfo]:
-        return self._factory(x)
+        return self._apply_aggregation(x) if self._is_aggregated else self._apply_single_metric(x)
 
     def single(self, x: TData) -> BaseSolutionQualityInfo:
-        result, *_ = self._factory([x])
+        """
+        Generates quality metric for single data instance
+        :param x: Single data instance
+        :return: Solution quality for data instance
+        """
+        [result] = self.__call__([x])
         return result
+
+    def add_metrics(self, *metrics: Callable[[list[TData]], list[BaseSolutionQualityInfo]]):
+        if not self._is_aggregated:
+            raise Exception('Cannot add metrics to not aggregating factory.')
+
+        self._metrics.extend(metrics)
+
+    def aggregate(self, metrics_aggregation: Callable[[Iterable[Iterable[BaseSolutionQualityInfo]]],
+                                                      Iterable[BaseAggregatedSolutionQualityInfo]],
+                  *metrics: Callable[[list[TData]], list[BaseSolutionQualityInfo]]):
+        if self._is_aggregated:
+            raise Exception('Cannot add aggregation to an already aggregated factory.')
+        if not metrics:
+            raise Exception('Should add at least one metric for aggregation.')
+        self._is_aggregated = True
+        self._metrics_aggregation = metrics_aggregation
+        self._metrics.extend(metrics)
+
+    # TODO: think-off params and implement logics
+    def wrap_aggregate(self):
+        ...
+
+    def _apply_aggregation(self, x: list[TData]) -> Iterable[BaseSolutionQualityInfo]:
+        return self._metrics_aggregation(zip(*[m(x) for m in self._metrics]))
+
+    def _apply_single_metric(self, x: list[TData]) -> Iterable[BaseSolutionQualityInfo]:
+        [metric] = self._metrics
+        return metric(x)
