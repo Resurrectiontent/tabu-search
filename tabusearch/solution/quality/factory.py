6390a890a24da18ee4f85e6e7c9b18351f9b1b44
resurrectiontent <asvoskresenskii@gmail.com>
2023-05-04 14:16:11 +0300

    finish quality factory extension

--- tabusearch/solution/quality/factory.py
+++ tabusearch/solution/quality/factory.py
@@ -1,3 +1,4 @@
+from functools import partial
 from typing import Callable, Iterable, Generic
 
 from tabusearch.solution.quality.aggregated import BaseAggregatedSolutionQualityInfo
@@ -35,22 +36,32 @@ class SolutionQualityFactory(Generic[TData]):
 
     def single(self, x: TData) -> BaseSolutionQualityInfo:
         """
-        Generates quality metric for single data instance
-        :param x: Single data instance
-        :return: Solution quality for data instance
+        Generates quality metric for single data instance.
+        :param x: Single data instance.
+        :return: Solution quality for data instance.
         """
         [result] = self.__call__([x])
         return result
 
     def add_metrics(self, *metrics: Callable[[list[TData]], list[BaseSolutionQualityInfo]]):
+        """
+        Adds more metrics to an aggregating solution quality factory.
+        :param metrics: New metrics.
+        """
         if not self._is_aggregated:
             raise Exception('Cannot add metrics to not aggregating factory.')
 
         self._metrics.extend(metrics)
 
-    def aggregate(self, metrics_aggregation: Callable[[Iterable[Iterable[BaseSolutionQualityInfo]]],
-                                                      Iterable[BaseAggregatedSolutionQualityInfo]],
-                  *metrics: Callable[[list[TData]], list[BaseSolutionQualityInfo]]):
+    def aggregate(self, *metrics: Callable[[list[TData]], list[BaseSolutionQualityInfo]],
+                  metrics_aggregation: Callable[[Iterable[Iterable[BaseSolutionQualityInfo]]],
+                                                Iterable[BaseAggregatedSolutionQualityInfo]]):
+        """
+        Makes a non-aggregating solution quality factory an aggregating one.
+        Adds more metrics and their aggregation.
+        :param metrics: New metrics to add.
+        :param metrics_aggregation: Aggregation for metrics.
+        """
         if self._is_aggregated:
             raise Exception('Cannot add aggregation to an already aggregated factory.')
         if not metrics:
@@ -59,12 +70,30 @@ class SolutionQualityFactory(Generic[TData]):
         self._metrics_aggregation = metrics_aggregation
         self._metrics.extend(metrics)
 
-    # TODO: think-off params and implement logics
-    def wrap_aggregate(self):
-        ...
+    def wrap_aggregate(self, *metrics: Callable[[list[TData]], list[BaseSolutionQualityInfo]],
+                       metrics_aggregation: Callable[[Iterable[Iterable[BaseSolutionQualityInfo]]],
+                                                     Iterable[BaseAggregatedSolutionQualityInfo]]):
+        """
+        Wraps old metrics and aggregations of an aggregating solution factory as a single metric
+        and adds new metrics along with the old "product" (aggregated) metric and an aggregation for them.
+        :param metrics: New metrics to be accounted along with the old aggregated metric.
+        :param metrics_aggregation: New aggregation to aggregate new metrics along with old aggregated metric.
+        """
+        if not self._is_aggregated:
+            raise Exception('Cannot wrap an non-aggregating solution quality factory. Use `aggregate` method instead.')
+        self._metrics = [partial(self._apply_aggregation_static, self._metrics, self._metrics_aggregation),
+                         *metrics]
+        self._metrics_aggregation = metrics_aggregation
 
     def _apply_aggregation(self, x: list[TData]) -> Iterable[BaseSolutionQualityInfo]:
-        return self._metrics_aggregation(zip(*[m(x) for m in self._metrics]))
+        return self._apply_aggregation_static(x, self._metrics, self._metrics_aggregation)
+
+    @staticmethod
+    def _apply_aggregation_static(x: list[TData],
+                                  metrics: Iterable[Callable[[list[TData]], list[BaseSolutionQualityInfo]]],
+                                  metrics_aggregation: Callable[[Iterable[Iterable[BaseSolutionQualityInfo]]],
+                                                                Iterable[BaseAggregatedSolutionQualityInfo]]):
+        return metrics_aggregation(zip(*[m(x) for m in metrics]))
 
     def _apply_single_metric(self, x: list[TData]) -> Iterable[BaseSolutionQualityInfo]:
         [metric] = self._metrics
